1c9623c23b41e08c0e1f79f0b85263727a056a9c
Wojtek <41506511+wojtekrat@users.noreply.github.com>
2018-08-05 17:53:40 +0200

    upgraded home section

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
-      $('h1').css({"opacity" : "0.2"});
+      $('#f1').css({"opacity" : "0.2"});
     } else {
-      $('h1').css({"opacity" : "1"});
+      $('#f1').css({"opacity" : "1"});
     }
   });
 });
